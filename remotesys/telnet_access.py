f0b892efdf5549a36b0ee3e940ad11aca13df93b
Diego J. Chacón <dchacon@lynx.com.ec>
2017-07-19 10:52:47 -0500

    Added Avaya SW telnet remote access

---
+++ remotesys/telnet_access.py
@@ -0,0 +1,31 @@
+import getpass
+import sys
+import telnetlib
+import time
+
+print()
+print(" --> Remote access to Avaya switches <--")
+print()
+TEL_HOST = input("Enter the Avaya switch IP address: ")
+TEL_USER = 'rwa'
+TEL_PASS = getpass.getpass()
+
+tn = telnetlib.Telnet(TEL_HOST)
+time.sleep(4)
+print("Reading login...")
+tn.read_until("Login: ".encode())
+time.sleep(1)
+tn.write((TEL_USER + "\n").encode())
+if TEL_PASS:
+    print("Reading password...")
+    tn.read_until("Password: ".encode())
+    tn.write((TEL_PASS + "\n").encode())
+print("Writting commands")
+tn.write("show ip interface\n".encode())
+time.sleep(1)
+print("Printing all")
+print(tn.read_very_eager())
+print("Exiting shit")
+tn.write("exit\n".encode())
+time.sleep(2)
+tn.close()
